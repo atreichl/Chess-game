a90527104c66e25adfe764fbc0c1e2e9840f13f1
Andrew Treichler <atreichl@asu.edu>
2026-03-22 12:58:27 -0500

    Start of a chess game. early UI test. Chess pieces from clipartmax.

---
+++ src/src/UserInterface.java
@@ -0,0 +1,61 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+
+public class UserInterface extends JPanel implements MouseListener, MouseMotionListener {
+    static int x,y;
+
+    @ Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        this.setBackground(Color.yellow);
+        this.addMouseListener(this);
+        this.addMouseMotionListener(this);
+        g.setColor(Color.red);
+        g.fillRect(10, 20, 100, 200);
+
+        Image chessPiecesSheet;
+        chessPiecesSheet = new ImageIcon("chessPiecesSheet.jpg").getImage();
+        g.drawImage(chessPiecesSheet,x,y,x+64,y+64,0,0,64,64,this);
+    }
+
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        x = e.getX();
+        y = e.getY();
+        repaint();
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+
+    }
+}
