811eaf925b5e5acf6ae8569d5f2bca3cdcdcd026
Andrew Treichler <atreichl@asu.edu>
2026-03-22 12:59:15 -0500

    Start of a chess game. early UI test. Chess pieces from clipartmax.

---
+++ src/src/Main.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        JFrame f = new JFrame("Chess");
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        UserInterface ui = new UserInterface();
+
+        f.add(ui);
+        f.setSize(500, 500);
+        f.setVisible(true);
+    }
+}
